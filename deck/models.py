20674f7a58057b32258933706e0611ddfa67ed06
The-Noble-K <jarod.sneed@gmail.com>
2019-07-19 21:07:44 -0400

    first commit. card model init.

---
+++ deck/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Card(models.Model):
+    name = models.CharField(max_length=120)
+    meaning = models.CharField(max_length=200)
+    reversal = models.CharField(max_length=200)
+    card_id = models.IntegerField()
+    image = models.ImageField()
+
+    def __str__(self):
+        return "%s \n %s \n %s \n %s \n %s \n" %(self.name, self.meaning, self.reversal, self.card_id, self.image)
