3f7989d4520699568bf75aef68f2c1ad8d1650dd
The-Noble-K <jarod.sneed@gmail.com>
2019-07-19 21:50:29 -0400

    added frontend, displaying data, image broken

---
+++ digitarot_frontend/src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    state = {
+        cards: []
+    };
+
+    async componentDidMount() {
+        try {
+            const response = await fetch('http://localhost:8001/api/');
+            const cards = await response.json();
+            this.setState({cards});
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    render() {
+        return(
+        <div>
+            {this.state.cards.map(card => (
+                <div key={card.id}>
+                    <h1>{card.name}</h1>
+                    <img src={card.image} alt={card.name} />
+                    <span>{card.meaning}</span>
+                    <span>{card.reversal}</span>
+                </div>
+            ))}
+        </div>
+        );
+    }
+}
+
+export default App;
