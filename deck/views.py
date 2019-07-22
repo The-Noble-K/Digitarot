91de2fb7f922c6dff029aecd69e619751a4ed977
The-Noble-K <jarod.sneed@gmail.com>
2019-07-22 16:57:49 -0400

    added major arcana

--- deck/views.py
+++ deck/views.py
@@ -1,12 +1,13 @@
 from rest_framework import generics
-
 from .models import Card
 from .serializers import CardSerializer
 
 class ListDeck(generics.ListCreateAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+    
 
 class DetailDeck(generics.RetrieveUpdateDestroyAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+
